89d48361671e693d7b4a70d0fe6fe53480babb9c
Victoria Trac <victoriatrac@gmail.com>
2021-03-21 17:49:19 -0600

    finished mvp

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,8 +1,14 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from "react"
+import axios from "axios"
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  constructor() {
+    super()
+    this.state = {
+      plants: []
+    }
+  }
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
@@ -12,7 +18,7 @@ export default class PlantList extends Component {
   componentDidMount() {
     axios.get('http://localhost:3333/plants')
       .then(res => {
-        console.log(res);
+        this.setState({ plants: res.data })
       });
   }
 
